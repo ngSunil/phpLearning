c1dce291fe8fc31a53427af59a1145b37aab6a9d
unknown <sun.aec@gmail.com>
2023-03-22 16:56:33 +0530

    till form validation and upload file

--- forms/formstudent.php
+++ forms/formstudent.php
@@ -1,29 +1,91 @@
+<?php
+session_start();
+$name_error= "";
+$email_error= "";
+$rollno_error= "";
+$class_error= "";
+$photo_error= "";
+
+
+
+
+$name_value= "";
+$email_value= "";
+$rollno_value= "";
+$class_value= "";
+$photo_value= "";
+
+
+if(isset($_SESSION['form'])){
+   $name_value= $_SESSION['form']['name'];
+   $email_value= $_SESSION['form']['email'];
+   $rollno_value= $_SESSION['form']['rollno'];
+   $class_value= $_SESSION['form']['class'];
+   unset($_SESSION['form']);
+}
+
+
+
+
+if (isset($_SESSION['error'])) {
+
+
+  if(isset($_SESSION['error']['name'])){
+   $name_error=$_SESSION['error']['name'];
+  }
+
+
+  if(isset($_SESSION['error']['email'])){
+   $email_error=$_SESSION['error']['email'];
+  }
+
+
+  if(isset($_SESSION['error']['rollno'])){
+   $rollno_error=$_SESSION['error']['rollno'];
+  }
+
+
+  if(isset($_SESSION['error']['class'])){
+   $class_error=$_SESSION['error']['class'];
+  }
+
+
+  if(isset($_SESSION['error']['photo'])){
+   $photo_error=$_SESSION['error']['photo'];
+  }
+
+
+  unset($_SESSION['error']);
+
+
+ 
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+
+
+   <style>
+       p{
+           color: red;
+       }
+   </style>
 </head>
-<body style="margin: 10px">
-<div class="col-md-6">
-    
-
-    <form action="submitform.php" enctype="multipart/form-data" method="post">
+<body>
+   <form action="submitform.php" enctype="multipart/form-data" method="post">
 
 
-    <div class="form-control">
-<label for="name">Name</label>
- <input class="form-control" type="text" name="name"><br>
- Email: <input type="text" class="form-control" name="email"><br>
- Roll No. <input type="text" class="form-control" name="rollnumber"><br>
- Class <input type="text" class="form-control" name="class"><br>
- Photo. <input type="file" class="form-control" name="photo"><br>
- <button class="btn btn-primary" type="submit">Submit Form</button>
-</div>
-    </form>
-    </div>
+       Name: <input type="text" value="<?php echo $name_value; ?>"   name="name" > <p> <?php echo $name_error; ?> </p> <br>
+       Email: <input type="text"  value="<?php echo $email_value; ?>"  name="email" > <p><?php echo $email_error; ?></p> <br>
+       Roll No. <input type="text"  value="<?php echo $rollno_value; ?>" name="rollno" > <p><?php echo $rollno_error; ?></p> <br>
+       Class: <input type="text" value="<?php echo $class_value; ?>"  name="class" > <p><?php echo $class_error; ?></p> <br>
+       photo: <input type="file" name="photo" > <p><?php echo $photo_error; ?></p> <br>
+       <input type="submit" value="submit">
+   </form>
 </body>
 </html>
--- forms/submitform.php
+++ forms/submitform.php
@@ -1,9 +1,50 @@
 <?php
-$dir = "uploads/";
-$filename = uniqid().$_FILES['photo']['name'];
-$target = $dir.$filename;
-if(move_uploaded_file($_FILES["photo"]["tmp_name"], $target)){
-echo "uploaded";
-} else {
-echo "error uploading";
+session_start();
+if( strlen($_POST['name']) < 3 ||   strlen($_POST['name']) >10 ){
+   $_SESSION['error']['name']="Enter a valid Name between 3 to 10 letters";
+}
+
+
+if( $_POST['email']=="" || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+   $_SESSION['error']['email']="Enter a valid Email";
+}
+
+
+if(strlen($_POST['rollno'])!=7){
+   $_SESSION['error']['rollno']="Roll no. should be of length 7";
+}
+
+
+if(!filter_var($_POST['class'], FILTER_VALIDATE_INT) || $_POST['class']<1 || $_POST['class']>10){
+   $_SESSION['error']['class']= "Enter class between 1 to 10";
+}
+
+
+if (isset($_FILES['photo']) && isset($_FILES['photo']['name']) && $_FILES['photo']['name']!= "") {
+ if($_FILES['photo']['size']< 500000){
+
+
+       $dir="uploads/";
+
+
+       $filename=uniqid(). $_FILES['photo']['name'];
+       $target= $dir.$filename;
+       if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target)) {
+          
+       }
+ }else{
+   $_SESSION['error']['photo']= "File size should be less than 500kb";
+ }
+}else{
+   $_SESSION['error']['photo']="Photo is required";
+}
+
+
+if (isset($_SESSION['error'])) {
+   $_SESSION['form']= $_POST;
+   header("Location: ".$_SERVER['HTTP_REFERER']);
+
+
+}else{
+   header("Location: http://localhost:8000/forms/success.php");
 }
